f10c5a220185d4fd9a2fc0dee9414106f62ee194
A.B. Carroll <ben@hl9.net>
2018-02-24 21:03:20 -0500

    Possible fix for #55: t:omit with t:block attr directly underneath

--- src/Node/OmitNode.php
+++ src/Node/OmitNode.php
@@ -4,6 +4,7 @@ namespace Goetas\Twital\Node;
 use Goetas\Twital\Node;
 use Goetas\Twital\Compiler;
 use Goetas\Twital\Helper\DOMHelper;
+use Goetas\Twital\Twital;
 
 /**
  *
@@ -14,8 +15,16 @@ class OmitNode implements Node
 {
     public function visit(\DOMElement $node, Compiler $context)
     {
+        $sandbox = $node->ownerDocument->createElementNS(Twital::NS, "sandbox");
+
+        $node->parentNode->insertBefore($sandbox, $node);
+        $node->parentNode->removeChild($node);
+        $sandbox->appendChild($node);
+
         $context->compileAttributes($node);
         $context->compileChilds($node);
+
+        DOMHelper::replaceWithSet($sandbox, iterator_to_array($sandbox->childNodes));
         DOMHelper::replaceWithSet($node, iterator_to_array($node->childNodes));
     }
 }
